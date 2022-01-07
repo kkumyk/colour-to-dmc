d5e6e57401501cae62f75f9517f81f4f55b39800
Karina Kumykova <k.kumykova@gmail.com>
2022-01-07 17:51:21 +0000

    overlay the colour palette on top of the image

--- src/colour_to_dmc/image.py
+++ src/colour_to_dmc/image.py
@@ -1,7 +1,7 @@
 import cv2
 import pandas as pd
 import numpy as np
-from colours import rgb_to_dmc
+from colours import rgb_to_dmc, dmc_colors
 from collections import Counter
 import matplotlib.pyplot as plt
 
@@ -23,7 +23,7 @@ bgr_tuple_array = [tuple(row) for row in bgr_concat_array]
 unique_bgr_array = np.unique(bgr_tuple_array, axis=0)
 
 # find the closest dmc colour using unique bgr values in the unique_bgr_array
-dmc_colours = [(rgb_to_dmc(c[2], c[1], c[0])) for c in unique_bgr_array]
+dmc_colours = [(rgb_to_dmc(c[2], c[1], c[0])) for c in unique_bgr_array] # RENAME the variable
 
 # dedupe colour_list
 unique_dmc_colours = [dict(t) for t in {tuple(d.items()) for d in dmc_colours}]
@@ -44,21 +44,48 @@ limit_low_occurring_threads = 2  # %
 filtered_floss_list = [
     color for color in floss_use_percentage if color[1] > limit_low_occurring_threads
 ]
+print('Number of most used threads: ', len(filtered_floss_list))
+
 filtered_floss_df = pd.DataFrame(filtered_floss_list).rename(columns={0: 'floss', 1: '%'})
 
 unique_dmc_df = pd.DataFrame(unique_dmc_colours)
-unique_dmc_df[["floss", "description"]]
+# unique_dmc_df[["floss", "description"]]
 
 merged_colours = pd.merge(filtered_floss_df, unique_dmc_df, how="left", on="floss").sort_values('%', ascending=False)
 dmc_palette = merged_colours[["floss", "description", "red", "green", "blue", "%"]]
 
-print(dmc_palette)
+
+print("DMC Palette", dmc_palette)
+
 # get the list of rgb combinations for each
 # http://net-informations.com/ds/pd/iterate.htm
 rgb_palette = [[x, y, z] for x, y, z in zip(dmc_palette['red'], dmc_palette['green'], dmc_palette['blue'])]
 
-
 plt.imshow([rgb_palette])
-plt.show()
+# plt.show()
 
+# overlay the color palette on top of the image
+_, w, _ = image.shape
+size = int(w / len(filtered_floss_list))
+y = size
+
+#
+# print("DMC COLORS", dmc_colors)
+#
+# for idx, color in enumerate(filtered_floss_list):
+#     b, g, r = (
+#         dmc_colors[color[0]]["blue"],
+#         dmc_colors[color[0]]["green"],
+#         dmc_colors[color[0]]["red"],
+#     )
+#     cv2.rectangle(
+#         image, (size * idx, 0), ((size * idx) + size, size), (b, g, r), -1
+#     )
+#
+#
+# print('Size: ', size)
+#
+# print('Image width: ', w)
+#
+# cv2.rectangle(image,(384,0),(510,128),(0,255,0),3)
 
