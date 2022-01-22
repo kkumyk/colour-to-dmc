e2da1eb0d9f95ccac02ab10adbd047cd09c955c4
Karina Kumykova <k.kumykova@gmail.com>
2022-01-22 15:43:14 +0000

    update description and print statement

--- src/colour_to_dmc/image.py
+++ src/colour_to_dmc/image.py
@@ -4,14 +4,14 @@ from PIL import Image
 
 def check_size_and_quantize(input_image, colour_limit):
     """
-    Check the size of the provided image and exits the program if not satisfied.
-    Else, quantizes the image.
+    Checks the size of the provided image and exits the program if
+    width < 1000px or height < 1100px. Else, quantizes the image.
     """
     image_to_quantize = Image.open(input_image)
     width, height = image_to_quantize.size
 
     if width < 1000 or height < 1100:
-        print("Please provide an image with a width of at least 1107px and a height of at least 1250px.")
+        print("Please provide an image with a width of at least 1000px and a height of at least 1100px.")
         sys.exit("The provided image is too small.")
     else:
         quantized_image = image_to_quantize.quantize(colour_limit)
