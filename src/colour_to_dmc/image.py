207727415b6468095c4cbcfdbe15640f1ed7a015
Karina Kumykova <k.kumykova@gmail.com>
2022-01-24 14:33:51 +0000

    check image format and exit if incorrect

--- src/colour_to_dmc/image.py
+++ src/colour_to_dmc/image.py
@@ -4,18 +4,25 @@ from PIL import Image
 
 def check_size_and_quantize(input_image, colour_limit):
     """
-    Checks the size of the provided image and exits the program if
-    width < 1000px or height < 1100px. Else, quantizes the image.
+    Checks the image type and the size of the provided image and exits the program if:
+    - the image is not in PNG or JPEG formats
+    - width < 1000px or height < 1100px.
+    Else, it quantizes the image.
     """
     image_to_quantize = Image.open(input_image)
-    width, height = image_to_quantize.size
 
-    if width < 1000 or height < 1100:
-        print("Please provide an image with a width of at least 1000px and a height of at least 1100px.")
-        sys.exit("The provided image is too small.")
+    if image_to_quantize.format == 'JPEG' or image_to_quantize.format == 'PNG':
+        width, height = image_to_quantize.size
+
+        if width < 1000 or height < 1100:
+            print("Please provide an image with a width of at least 1000px and a height of at least 1100px.")
+            sys.exit("The provided image is too small.")
+
+        else:
+            # add check for colour_limit
+            quantized_image = image_to_quantize.quantize(colour_limit)
+        return quantized_image
 
     else:
-        # add check for colour_limit
-        quantized_image = image_to_quantize.quantize(colour_limit)
-
-    return quantized_image
+        print("Please provide a valid image in one of the following formats: PNG or JPEG.")
+        sys.exit("The provided image is of the following format: " + image_to_quantize.format)
