a598729d4552385ebfe0fdaa258048fa2532373d
Karina Kumykova <k.kumykova@gmail.com>
2022-01-24 12:43:30 +0000

    use pytest.mark.parametrize decorator to combine tests

--- tests/test_image.py
+++ tests/test_image.py
@@ -2,14 +2,11 @@ import pytest
 from colour_to_dmc.image import check_size_and_quantize
 
 
-def test_check_size_and_quantize_small_size():
-    #input_image = 'flowers.png'  # Image size: 225x225 pixels
-    input_image = 'flowers.png'  # Image size: 225x225 pixels
-    colour_limit = 255
-
+@pytest.mark.parametrize("expected_exception, input_image, colour_limit",
+                         [(SystemExit, 'yellow-flower.jpeg', 255),
+                          (SystemExit, 'irises.jpeg', 255)])
+def test_check_size_and_quantize_small_size(expected_exception, input_image, colour_limit):
     with pytest.raises(SystemExit) as e:
         check_size_and_quantize(input_image, colour_limit)
     assert e.type == SystemExit
     assert e.value.code == 'The provided image is too small.'
-
-
