e0abd06a5c7f4009d6a560c98151818b120aed7e
Karina Kumykova <k.kumykova@gmail.com>
2022-01-25 15:18:36 +0000

    check the given number for colour_limit

--- src/colour_to_dmc/image.py
+++ src/colour_to_dmc/image.py
@@ -9,21 +9,30 @@ def check_size_and_quantize(input_image, colour_limit):
     - width < 1000px or height < 1100px.
     Else, it quantizes the image.
     """
-    image_to_quantize = Image.open(input_image)
-    file_format = image_to_quantize.format
+    if colour_limit < 1:
+        print("Please choose the number of colours to use in the image. It should between 1 and 255.")
+        sys.exit("The given number is too small.")
 
-    if file_format == 'JPEG' or file_format == 'PNG':
-        width, height = image_to_quantize.size
-
-        if width < 1000 or height < 1100:
-            print("Please provide an image with a width of at least 1000px and a height of at least 1100px.")
-            sys.exit("The provided image is too small.")
-
-        else:
-            # add check for colour_limit
-            quantized_image = image_to_quantize.quantize(colour_limit)
-        return quantized_image
+    elif colour_limit > 255:
+        print("Please choose the number of colours to use in the image. It should between 1 and 255.")
+        sys.exit("The given number is too big.")
 
     else:
-        print("Please provide a valid image in one of the following formats: PNG or JPEG.")
-        sys.exit("The provided image should be in PNG or JPEG format.")
+
+        image_to_quantize = Image.open(input_image)
+        file_format = image_to_quantize.format
+
+        if file_format == 'JPEG' or file_format == 'PNG':
+            width, height = image_to_quantize.size
+
+            if width < 1000 or height < 1100:
+                print("Please provide an image with a width of at least 1000px and a height of at least 1100px.")
+                sys.exit("The provided image is too small.")
+
+            else:
+                quantized_image = image_to_quantize.quantize(colour_limit)
+            return quantized_image
+
+        else:
+            print("Please provide a valid image in one of the following formats: PNG or JPEG.")
+            sys.exit("The provided image should be in PNG or JPEG format.")
