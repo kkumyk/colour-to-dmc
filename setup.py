3cd7439f77d3fa7b163fb424244ad3af5b943295
Karina Kumykova <k.kumykova@gmail.com>
2022-01-27 17:09:35 +0000

    delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='colour_to_dmc',
-    extras_require=dict(tests=['pytest', 'scipy', 'opencv-python', 'pandas', 'numpy', 'matplotlib']),
-    packages=find_packages(where='src'),
-    package_dir={"": "src"},
-
-)
