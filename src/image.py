d4ab861db6cd7ac19776932030374c4b39473542
Karina Kumykova <k.kumykova@gmail.com>
2022-01-28 12:06:21 +0000

    remove the width restriction for the input image

--- src/image.py
+++ src/image.py
@@ -6,7 +6,7 @@ def check_size_and_quantize(input_image, colour_limit):
     """
     Checks the image type and the size of the provided image and exits the program if:
     - the image is not in PNG or JPEG formats
-    - width < 1000px or height < 1100px.
+    - height < 1000px.
     Else, it quantizes the image.
     """
     if colour_limit < 1:
@@ -18,15 +18,14 @@ def check_size_and_quantize(input_image, colour_limit):
         sys.exit("The given number is too large.")
 
     else:
-
         image_to_quantize = Image.open(input_image)
         file_format = image_to_quantize.format
 
         if file_format == 'JPEG' or file_format == 'PNG':
             width, height = image_to_quantize.size
 
-            if width < 1000 or height < 1100:
-                print("Please provide an image with a width of at least 1000px and a height of at least 1100px.")
+            if height < 1000:
+                print("Please provide an image with a height of at least 1000px.")
                 sys.exit("The provided image is too small.")
 
             else:
